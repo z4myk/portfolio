11d40204b543472b46f928cb1cc3559facb1455e
Sebastian Mosquera <sebimosquera@gmail.com>
2022-05-24 18:08:09 -0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import '../App.css';
+export const Navbar = () => {
+  return (
+    <>
+    <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SM</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0 mx-5 ms-auto">
+        <li class="nav-item">
+          <a class="nav-link"  aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Skills</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contáctame</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  );
+};
