86740349637dfd9a2b67eb7588bdf3ae52aed14d
Sebastian Mosquera <sebimosquera@gmail.com>
2023-01-06 17:25:59 -0300

    se arreglo cards de la section de skills

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -1,68 +1,149 @@
-import React from 'react'
-import eccomerce from "../assets/eccomerce.png"
-import veterinarycrud from '../assets/veterinarycrud.png'
-import rollingnew from '../assets/rollingnew.png'
-import heroapp from '../assets/heroapp.png'
+import React from "react";
+import eccomerce from "../assets/eccomerce.png";
+import veterinarycrud from "../assets/veterinarycrud.png";
+import rollingnew from "../assets/rollingnew.png";
+import heroapp from "../assets/heroapp.png";
 export const Proyects = () => {
-    return (
-        <div>
-            <section className="container">
-          <h2 className="my-5 mb-2 text-center" id="proyectos">Proyectos</h2>
-          <div className="d-flex justify-content-around mb-5">
-            <hr className="w-25"/>
-            <hr className="w-25"/>
+  return (
+    <div>
+      <section className="container">
+        <h2 className="my-5 mb-2 text-center" id="proyectos">
+          Proyectos
+        </h2>
+        <div className="d-flex justify-content-around mb-5">
+          <hr className="w-25" />
+          <hr className="w-25" />
         </div>
         <div className="d-flex justify-content-center flex-wrap ">
-
-        <div className="cardcito mx-3 mb-3">
+          <div className="cardcito mx-3 mb-3">
             <div className="card2">
-              <img src={eccomerce} alt="tienda digital" className="w-100 imagenProyecto" />
-              <p className="container my-2 text-light text-center">E-ccomerce con autenticación desarrollado con React, Bootstrap, y auth0</p>
+              <img
+                src={eccomerce}
+                alt="tienda digital"
+                className="w-100 imagenProyecto"
+              />
+              <p className="container my-2 text-light text-center">
+                E-ccomerce con autenticación desarrollado con React, Bootstrap,
+                y auth0
+              </p>
               <div className="container">
-            <a  target="_blank"  rel="noreferrer" href="https://github.com/z4myk/e-ccomerce-reactjs"><button className="botonProyecto mx-3 text-light">Github</button></a>
-            <a  target="_blank"  rel="noreferrer" href="https://eccomercesam.netlify.app/"><button className="botonProyecto text-light">Deploy</button></a>
-
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/z4myk/e-ccomerce-reactjs"
+                >
+                  <button className="botonProyecto mx-3 text-light">
+                    Github
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://eccomercesam.netlify.app/"
+                >
+                  <button className="botonProyecto text-light">Deploy</button>
+                </a>
               </div>
             </div>
-        </div>    
-        <div className="cardcito mx-3 mb-3">
+          </div>
+          <div className="cardcito mx-3 mb-3">
             <div className="card2">
-            <img src={veterinarycrud} alt="tienda digital" className="w-100 imagenProyecto" />
-              <p className="container my-2 text-light text-center">Sistema de gestión de turnos para veterinaria CRUD con autenticación creado con React, Bootstrap y Auth0</p>
+              <img
+                src={veterinarycrud}
+                alt="tienda digital"
+                className="w-100 imagenProyecto"
+              />
+              <p className="container my-2 text-light text-center">
+                Sistema de gestión de turnos para veterinaria CRUD con
+                autenticación creado con React, Bootstrap y Auth0
+              </p>
               <div className="container">
-              <a  target="_blank"  rel="noreferrer" href="https://github.com/z4myk/crud-veterinary"><button className="botonProyecto mx-3 text-light">Github</button></a>
-            <a  target="_blank" rel="noreferrer"  href="https://veterinarycrud.netlify.app/"><button className="botonProyecto text-light">Deploy</button></a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/z4myk/crud-veterinary"
+                >
+                  <button className="botonProyecto mx-3 text-light">
+                    Github
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://veterinarycrud.netlify.app/"
+                >
+                  <button className="botonProyecto text-light">Deploy</button>
+                </a>
               </div>
             </div>
-        </div>    
-        <div className="cardcito mx-3 mb-3">
+          </div>
+          <div className="cardcito mx-3 mb-3">
             <div className="card2">
-            <img src={rollingnew} alt="tienda digital" className="w-100 imagenProyecto" />
-              <p className="container my-2 text-light text-center">Sitio web de noticias autoadministrable con React, Node js, express y Mongo DB</p>
+              <img
+                src={rollingnew}
+                alt="tienda digital"
+                className="w-100 imagenProyecto"
+              />
+              <p className="container my-2 text-light text-center">
+                Sitio web de noticias autoadministrable con React, Node js,
+                express y Mongo DB
+              </p>
               <div className="container">
-              <a  target="_blank"  rel="noreferrer" href="https://github.com/z4myk/RollingCodeNew/tree/master"><button className="botonProyecto mx-3 text-light">Github</button></a>
-            <a  target="_blank"  rel="noreferrer" href="https://rollingnewsg2.netlify.app/"><button className="botonProyecto text-light">Deploy</button></a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/z4myk/RollingCodeNew/tree/master"
+                >
+                  <button className="botonProyecto mx-3 text-light">
+                    Github
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://rollingnewsg2.netlify.app/"
+                >
+                  <button className="botonProyecto text-light">Deploy</button>
+                </a>
               </div>
             </div>
-        </div>   
-        <div className="cardcito mx-3 mb-3">
+          </div>
+          <div className="cardcito mx-3 mb-3">
             <div className="card2">
-            <img src={heroapp} alt="tienda digital" className="w-100 imagenProyecto" />
-              <p className="container my-2 text-light text-center">Sitio web buscador de heroes con React, redux y firebase.</p>
+              <img
+                src={heroapp}
+                alt="tienda digital"
+                className="w-100 imagenProyecto"
+              />
+              <p className="container my-2 text-light text-center">
+                Sitio web buscador de heroes con React, redux y firebase.
+              </p>
               <div className="container">
-              <a  target="_blank"  rel="noreferrer" href="https://github.com/z4myk/HeroApp"><button className="botonProyecto mx-3 text-light">Github</button></a>
-            <a  target="_blank"   rel="noreferrer" href="https://reactheroapp.netlify.app/"><button className="botonProyecto text-light">Deploy</button></a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/z4myk/HeroApp"
+                >
+                  <button className="botonProyecto mx-3 text-light">
+                    Github
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://reactheroapp.netlify.app/"
+                >
+                  <button className="botonProyecto text-light">Deploy</button>
+                </a>
               </div>
             </div>
-        </div>   
-        {/* <div className="cardcito mx-3">
+          </div>
+          {/* <div className="cardcito mx-3">
             <div className="card2">
             </div>
         </div>     */}
-
         </div>
-        </section>
-        
-        </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -24,44 +24,44 @@ export const Skills = () => {
         <hr className="w-25"/>
         </div>
         <div className="responsiveMobile">
-          <div className="d-flex flex-wrap gap-3 justify-content-center mx-5 ">
-            <div className=" glass mb-3">
+          <div className="d-flex flex-wrap gap-3 justify-content-between mx-5 ">
+            <div className=" glass mb-3 mb-2">
               <img  src={html} className=" logoSkill"  alt="html" />
               <span className="mx-4">HTML5</span>
             </div>
-            <div className=" glass mb-3">
+            <div className=" glass mb-3 mb-2">
               <img  src={css} className=" logoSkill " alt="css" />
               <span className="mx-4">CSS</span>
             </div>
-            <div className=" glass mb-3">
+            <div className=" glass mb-3 mb-2">
               <img  src={bootstrap} className=" logoSkill " alt="bootstrap" />
               <span className="mx-4">Bootstrap</span>
             </div>
-            <div className=" glass mb-3"> 
+            <div className=" glass mb-3 mb-2"> 
             <img  src={javascript} className=" javascript" alt="javascript"  />
             <span className="mx-1">javascript</span>
             </div>
-            <div className="  glass mb-3">
+            <div className="  glass mb-3 mb-2">
             <img  src={react} className=" logoSkill" alt="react" />
             <span className="mx-4">React</span>
             </div>  
-            <div className=" glass mb-3">
+            <div className=" glass mb-3 mb-2">
             <img  src={git} className="logoSkill"  alt="git" />
             <span className="mx-3">GIT</span>
             </div>
-            <div className=" glass mb-3">
+            <div className=" glass mb-3 mb-2">
             <img  src={nodejs} className=" javascript" alt="javascript"  />
             <span>Node JS</span>
             </div>  
-            <div className=" glass mb-3">
+            <div className=" glass mb-3 mb-2">
             <img  src={mongodb} className=" mongo" alt="mongodb" />
             <span>Mongo DB</span>
             </div>
-            <div className=" glass mb-3">
+            <div className=" glass mb-3 mb-2">
             <img  src={firebase} className=" firebase" alt="firebase" />
             <span>Firebase</span>
             </div>
-            <div className=" glass mb-3">
+            <div className=" glass mb-5">
             <img  src={photoshop} className=" photoshop"  alt="photoshop cs6" />
             <span className="mx-4">Photoshop</span>
             </div>  
