84e07660614e6a173f5879bc74de629c5f054e79
Sebastian Mosquera <sebimosquera@gmail.com>
2024-10-15 01:03:47 -0300

    add href goreboosting

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -289,7 +289,7 @@ export const Proyects = () => {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href=""
+                  href="https://goreboostingv2.netlify.app/"
                 >
                   <button className="botonProyecto mx-3 text-light">
                     Github
